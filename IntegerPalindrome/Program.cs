32e5e8d102cbddc270f9abe9c72ac460e1b9fa12
jennyf19 <ferriesj@gmail.com>
2017-07-20 17:30:58 -0700

    initial commit...again!

---
+++ IntegerPalindrome/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace IntegerPalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter a number: ");
+
+                Int16 input;
+                Int16.TryParse(Console.ReadLine(), out input);
+                Console.WriteLine("You entered: " + input.ToString());
+
+                bool reversedNum = isPalindrome(input);
+
+                if(reversedNum == false)
+                {
+                    Console.WriteLine("Not a palindrome: " + "\n" + input);
+                }
+                else 
+                {
+                    Console.WriteLine(input + "\nIs a palindrome!!");
+                }
+                Console.ReadLine();
+            }
+        }
+
+        public static bool isPalindrome(int x)
+        {
+            // check for negative number
+            if (x < 0)
+            {
+                return false;
+            }
+
+            int ReverseNum = 0;
+            int temp = 0;
+
+            temp = x;
+
+            try
+            {
+                while (x > 0)
+                {
+                    ReverseNum = checked(ReverseNum * 10) + (x % 10);
+                    x = x / 10;
+                }
+            }
+            catch (OverflowException)
+            {
+                return false;
+            }
+
+            if(temp == ReverseNum)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ LeetCode/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace LeetCodeReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter a number: ");
+
+                Int16 input;
+                Int16.TryParse(Console.ReadLine(), out input);
+                Console.WriteLine("You entered: " + input.ToString());
+
+                int reversedNum = Reverse(input);
+                Console.WriteLine("Number Reversed: " + reversedNum.ToString());
+                Console.ReadLine();
+            }
+        }
+
+        public static int Reverse(int x)
+        {
+            // check for negative number
+            bool negNum = false;
+            if (x < 0)
+            {
+                x = 0 - x;
+                negNum = true;
+            }
+
+            int ReverseNum = 0;
+            try
+            {
+                while (x > 0)
+                {
+                    ReverseNum = checked(ReverseNum * 10) + (x % 10);
+                    x = x / 10;
+                }
+
+                if (negNum)
+                {
+                    ReverseNum = 0 - ReverseNum;
+                }
+            }
+            catch (OverflowException)
+            {
+                return 0;
+            }
+
+            return ReverseNum;
+        }
+    }
+}
